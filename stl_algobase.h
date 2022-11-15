51fd111f2ea1c259309b3fd6cfdb7a9a556691c9
Miraboreas <88102238+Miraboreas@users.noreply.github.com>
2022-11-15 16:54:00 +0800

    Upgrade stl_algobase.h 2

--- stl_algobase.h
+++ stl_algobase.h
@@ -315,6 +315,13 @@ copy_n(_InputIter __first, _Size __count, _OutputIter __result) {
   return __copy_n(__first, __count, __result);
 }
 
+template <class _InputIter, class _Size, class _OutputIter>
+inline pair<_InputIter, _OutputIter>
+__copy_n(_InputIter __first, _Size __count, _OutputIter __result) {
+  return __copy_n(__first, __count, __result,
+                  __ITERATOR_CATEGORY(__first));
+}
+
 // fill and fill_n
 
 template <class _ForwardIter, class _Tp>
@@ -426,6 +433,20 @@ bool lexicographical_compare(_InputIter1 __first1, _InputIter2 __last1,
     if (*__first1 < *__first2)
       return true;
     if (*__first2 < *__first1)
+      return false;
+  }
+  return __first1 == __last1 && __first2 != __last2;
+}
+
+template <class _InputIter1, class _InputIter2, class _Compare>
+bool lexicographical_compare(_InputIter1 __first1, _InputIter2 __last1,
+                             _InputIter2 __first2, _InputIter2 __last2
+                             _Compare __comp) {
+  for ( ; __first1 != __last1 && __first2 != __last2
+        ; ++__first1, ++__first2) {
+    if (__comp(*__first1, *__first2))
+      return true;
+    if (__comp(*__first2, *__first1))
       return false;
   }
   return __first1 == __last1 && __first2 != __last2;
