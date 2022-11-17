31d63f5f93e673cc5027fc927c58120bffe9972c
Miraboreas <88102238+Miraboreas@users.noreply.github.com>
2022-11-18 02:34:15 +0800

    Update stl_config.h

--- stl_config.h
+++ stl_config.h
@@ -4,6 +4,7 @@
 #define __STL_BEGIN_NAMESPACE namespace __STL {
 #define __STL_END_NAMESPACE }
 #define __STL_TEMPLATE_NULL template<>
+#define __STL_DEFAULT_ALLOCATOR alloc
 #define __STL_TRY try
 #define __STL_UNWIND(action) catch(...) { action; throw; }
 #endif /* __STL_CONFIG_H */
